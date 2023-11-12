32c6c1b1a91c75d04e343b437b715315333a7493
Jose Silva <js38902@gmail.com>
2023-11-11 21:31:52 -0500

    added html, css, and javascript notes

---
+++ Assets/script.js
@@ -0,0 +1,16 @@
+var apiKey = "403dc4c9851a435ee53582f475dfb088";
+var apiBaseurl = "https://api.openweathermap.org/"
+var apiGeocode = "geo/1.0/direct?q={city name}&limit={limit}&appid={API key}";
+
+//add event listener to search btn, then grab data from input field
+
+//use data and fetch geo endpoint using the city that user enters with api key
+
+//render data on page into the current day
+
+//city gets added to local storage and render it also create a btn
+//after refresh make sure city names are saved still
+
+//create another function for 5 day forcast
+//render the 5 day forcast
+//filter out data for one time a day
